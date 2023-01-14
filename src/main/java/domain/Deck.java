4f9a5963091a1beb7478c5cdf13ac12347d25107
deserve82 <inth8204@gmail.com>
2023-01-14 19:34:21 +0900

    feat : deck 도메인 구현

---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,30 @@
+package domain;
+
+import domain.Card.Card;
+import domain.Card.Denomination;
+import domain.Card.Suit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Deck {
+
+    private final ArrayList<Card> cards = new ArrayList<>();
+
+    public Deck() {
+        Arrays.stream(Denomination.values()).forEach(this::addCard);
+        Collections.shuffle(cards);
+    }
+
+    private void addCard(Denomination denomination) {
+        Arrays.stream(Suit.values()).forEach(suit -> cards.add(new Card(denomination, suit)));
+    }
+
+    public int size() {
+        return cards.size();
+    }
+
+    public Card pop() {
+        return cards.remove(size() - 1);
+    }
+}
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,26 @@
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import domain.Card.Card;
+import domain.Deck;
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+
+    @Test
+    void create() {
+        assertDoesNotThrow(Deck::new);
+    }
+
+    @Test
+    void size() {
+        Deck deck = new Deck();
+        assertThat(deck.size()).isEqualTo(52);
+    }
+
+    @Test
+    void pop() {
+        Deck deck = new Deck();
+        assertThat(deck.pop() != null).isTrue();
+    }
+}
